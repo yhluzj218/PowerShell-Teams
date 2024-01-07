ce777bdcc5a63d01d3ca412093e78d6e9ff6336f
Connie Lu <153282304+yhluzj218@users.noreply.github.com>
2024-01-07 19:55:08 +0800

    Update AddMembersToTeam.ps1

--- AddMembersToTeam.ps1
+++ AddMembersToTeam.ps1
@@ -1,7 +1,22 @@
-# 獲取當前目錄下的所有文件
-$files = Get-ChildItem -Path .
+# 若要安裝模組的最新公開版本
+Install-Module -Name Teams
+#連結Teams
+Connect-MicrosoftTeams
+#連結Exchange
+Connect-ExchangeOnline
 
-# 遍歷並打印每個文件的名稱
-foreach ($file in $files) {
-    Write-Output $file.Name
+# Import the required module
+Import-Module -Name Teams
+
+# Define the team name
+$teamName = "營運國外出差日報 Y24Q1"
+
+# Get the team's Group ID
+$team = Get-Team -DisplayName $teamName
+$groupId = $team.GroupId
+
+$TeamUsers = Import-Csv -Path "C:\Temp\test1.csv"
+$TeamUsers | ForEach-Object {
+    Add-TeamUser -GroupId $groupId -User $_.Email
+    Write-host "Added User:"$_.Email -f Green
 }
